c26c1cabbdd415e39b775ebef1fb96f967782340
oran <ob2g19@soton.ac.uk>
2023-10-13 10:49:01 +0100

    Updated ClientMain to handle multiple commands

--- COMP2207Coursework/src/Client/ClientMain.java
+++ COMP2207Coursework/src/Client/ClientMain.java
@@ -6,34 +6,32 @@ import java.util.Scanner;
 public class ClientMain {
 
     public static void main(String[] args) throws InterruptedException, IOException {
-        String[] commands = {"STORE test1.txt 28", "STORE test2.txt 41", "LOAD test1.txt"};
-        for (String command : commands) {
-            new Thread(() -> {
-                try {
-                    new Client().run(command, args[0]);
-                } catch (IOException e) {
-                    System.out.println("ERROR");
-                    e.printStackTrace();
-                }
-            }).start();
-        }
-        Thread.sleep(2000);
-        new Client().run("LIST", args[0]);
+        System.out.println("Welcome to my Distributed File System");
+        System.out.println("-------------------------------------");
+        System.out.println("Commands List:");
+        System.out.println("STORE filename filesize");
+        System.out.println("REMOVE filename");
+        System.out.println("LOAD filename");
+        System.out.println("LIST");
+        System.out.println("-------------------------------------");
+        System.out.println("--- Enter commands below (to exit type QUIT) ---");
 
         Scanner s = new Scanner(System.in);
-        String[] commands2 = {"REMOVE test1.txt", "LOAD test1.txt"};
-        for (String command : commands2) {
-            new Thread(() -> {
-                try {
-                    new Client().run(command, args[0]);
-                } catch (IOException e) {
-                    System.out.println("ERROR");
-                    e.printStackTrace();
+        boolean end = false;
+        while (!end) {
+            try {
+                // Gets next command
+                String line = s.nextLine();
+                end = line.equals("QUIT");
+                if (!end) {
+                    // If not quitting, run command using Client object
+                    // args[0] are the arguments 'cport' and 'timeout' for Client
+                    new Client().run(s.nextLine(), args[0]);
                 }
-            }).start();
-        }
-        while (true) {
-            new Client().run(s.nextLine(), args[0]);
+            } catch (IOException e) {
+                System.out.println("ERROR");
+                e.printStackTrace();
+            }
         }
 
 
