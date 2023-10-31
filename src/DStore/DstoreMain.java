cd1ed709a164de25b5710b3b8ee2773ff71372b1
oran <ob2g19@soton.ac.uk>
2023-10-31 12:07:27 +0000

    Changed DstoreMain for better command line arguments

--- src/DStore/DstoreMain.java
+++ src/DStore/DstoreMain.java
@@ -8,13 +8,18 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class DstoreMain {
 
+    // DstoreMain cport timeout N
     public static void main(String[] args) throws IOException {
         DstoreLogger.init(Logger.LoggingType.ON_TERMINAL_ONLY, 5000);
-        for (int x = 1; x <= 5; x++) {
+        int cport = Integer.parseInt(args[0]);
+        int timeout = Integer.parseInt(args[1]);
+        int N = Integer.parseInt(args[2]);
+
+        for (int x = 1; x <= N; x++) {
             AtomicInteger a = new AtomicInteger(x);
             new Thread( () -> {
                 try {
-                    new Dstore(500 + a.get(), Integer.parseInt(args[0]), 5000, "Store" + a.get());
+                    new Dstore(500 + a.get(), cport, timeout, "Store" + a.get());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
